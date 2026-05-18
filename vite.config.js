17a3e82af2762dbc621570dfd574b7bbc05973b4
Shop-TH <darkstudio.master@gmail.com>
2026-05-18 21:40:01 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/skinmart-miniapp/",  // ← ชื่อ GitHub repo
+  base: "/shop-th/",  // ← ชื่อ GitHub repo
   build: {
     outDir: "dist",
   },
